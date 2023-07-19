e73b441160bee66e4bb1f68e9b01d34060cc625f
jsan502 <job.santos924@gmail.com>
2023-07-20 10:19:45 +1200

    feat: replace p elements with Typography

--- src/components/Sunstats.js
+++ src/components/Sunstats.js
@@ -1,28 +1,29 @@
 import React from "react";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Typography } from "@mui/material";
 
 const Sunstats = ({ weatherData }) => {
   return typeof weatherData.main !== "undefined" ? (
     <Grid container spacing={2}>
       <Grid item xs={2}>
-        <p>Sunrise</p>
-        <p>
+        <Typography>Sunrise</Typography>
+        <Typography>
           {weatherData.sys.sunrise !== undefined
             ? new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString(
                 "en-IN"
               )
             : "Sunrise not found."}
-        </p>
+        </Typography>
       </Grid>
       <Grid item xs={2}>
-        <p>Sunset</p>
-        <p>
+        <Typography>Sunset</Typography>
+        <Typography>
           {weatherData.sys.sunset !== undefined
             ? new Date(weatherData.sys.sunset * 1000).toLocaleTimeString(
                 "en-IN"
               )
             : "Sunset not found."}
-        </p>
+        </Typography>
       </Grid>
     </Grid>
   ) : (
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,36 +1,37 @@
 import React from "react";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import moment from "moment";
+import Typography from "@mui/material/Typography";
 
 const Weather = ({ weatherData }) => {
   return typeof weatherData.main !== "undefined" ? (
     <Grid container spacing={2}>
       <Grid item xs={2}>
-        <p>Day: {moment().format("dddd")}</p>
-        <p>
+        <Typography>Day: {moment().format("dddd")}</Typography>
+        <Typography>
           Temprature:{" "}
           {typeof weatherData.main.temp !== "undefined"
             ? weatherData.main.temp
             : "Temp not found."}{" "}
           &deg;C
-        </p>
-        <p>time</p>
-        <p>Date: {moment().format("LL")}</p>
-        <p>
+        </Typography>
+        <Typography>time</Typography>
+        <Typography>Date: {moment().format("LL")}</Typography>
+        <Typography>
           Humidity:{" "}
           {typeof weatherData.main.humidity !== "undefined"
             ? weatherData.main.humidity
             : "Humidity not found."}{" "}
-        </p>
+        </Typography>
       </Grid>
       <Grid item xs={2}>
-        <p>sunny icon</p>
-        <p>
+        <Typography>sunny icon</Typography>
+        <Typography>
           Description:{" "}
           {typeof weatherData.weather[0].main !== "undefined"
             ? weatherData.weather[0].main
             : "Weather Description not found."}
-        </p>
+        </Typography>
       </Grid>
     </Grid>
   ) : (
